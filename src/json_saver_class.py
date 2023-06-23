cc7069bf404259ad601bab231eb84647399ffd02
Zotov <ZotovDendos@yandex.ru>
2023-06-23 14:46:11 +0300

    Changed JSONSaver class

--- src/json_saver_class.py
+++ src/json_saver_class.py
@@ -9,11 +9,12 @@ class JSONSaver:
     vacancies = []
 
     def save_vacancies(self, data):
-
+        """Метод, сохраняющий в файл информацию по вакансиям"""
         with open("my_vacancies.json", "a", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False)
 
     def load_vacancies(self):
+        """Метод для чтения JSON данных из файла и преобразования их в словарь Python"""
         try:
             with open("my_vacancies.json", "r", encoding="utf-8") as file:
                 self.vacancies = json.load(file)
@@ -22,4 +23,5 @@ class JSONSaver:
             self.vacancies = []
 
     def add_vacancies(self, data):
+        """Метод для добавления вакансии в список вакансий"""
         self.vacancies.append(data)
